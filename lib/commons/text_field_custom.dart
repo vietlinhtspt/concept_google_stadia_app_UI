f0618da6d97b7d7acd7544a962d8379bd7da2055
LinhNV <vietlinhtspt@gmail.com>
2021-07-09 16:07:29 +0700

    With signUp page

--- lib/commons/text_field_custom.dart
+++ lib/commons/text_field_custom.dart
@@ -4,10 +4,13 @@ class TextFieldCustom extends StatelessWidget {
   final String labelText;
   final double width, height;
   final EdgeInsetsGeometry padding;
+  final bool obscureText, isValidInput;
   const TextFieldCustom({
     Key key,
     @required TextEditingController passwordController,
     this.labelText,
+    this.obscureText = false,
+    this.isValidInput = false,
     this.width = 300,
     this.height = 40,
     this.padding,
@@ -24,13 +27,22 @@ class TextFieldCustom extends StatelessWidget {
         width: this.width,
         height: this.height,
         child: TextField(
-          controller: this._passwordController,
-          obscureText: true,
-          decoration: InputDecoration(
-            border: OutlineInputBorder(),
-            labelText: this.labelText ?? "",
-          ),
-        ),
+            controller: this._passwordController,
+            obscureText: this.obscureText,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: this.labelText ?? "",
+              focusedBorder: this.isValidInput
+                  ? OutlineInputBorder(
+                      borderSide: BorderSide(
+                          color: Colors.greenAccent[100], width: 2.0),
+                    )
+                  : OutlineInputBorder(
+                      borderSide:
+                          BorderSide(color: Colors.redAccent, width: 2.0),
+                    ),
+              // errorText: this.isValidInput ? "" : "Invalid input"),
+            )),
       ),
     );
   }
