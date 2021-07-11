2fd83a0ec0dcfc87f6d534ca43909933671af288
LinhNV <vietlinhtspt@gmail.com>
2021-07-11 17:24:01 +0700

    Add Hero Animation

--- lib/pages/authenication_pages/authentication_pages.dart
+++ lib/pages/authenication_pages/authentication_pages.dart
@@ -33,9 +33,12 @@ class _AuthenticationPagesState extends State<AuthenticationPages> {
               Expanded(
                 child: Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 60),
-                  child: Image.asset(
-                    stadia_logo_with_name,
-                    height: 100,
+                  child: Hero(
+                    tag: "stadia_logo_with_name",
+                    child: Image.asset(
+                      stadia_logo_with_name,
+                      height: 100,
+                    ),
                   ),
                 ),
               ),
